55930ff3a20941c543ca0bcda6694c9e1d8ef187
HARSHIL2023 <harshil.patel.cg@gmail.com>
2025-11-26 20:02:57 +0530

    level05 also half completed����

---
+++ level05/all.js
@@ -0,0 +1,58 @@
+//41
+
+function countWords(string){
+    let temp=string;
+    let arr=[];
+    arry=temp.split(' ');
+    console.log(arry.length);
+}
+
+//42
+
+function replace(string){
+    let temp=string;
+    temp=temp.split('a').join("e");
+    console.log(temp);
+
+}
+
+//43
+
+
+function check(string, char) {
+  if (string.includes(char)) {
+    console.log(`yes ${char} is present in ${string}`);
+  } else {
+    console.log(`no ${char} is not present in ${string}`);
+  }
+}
+
+//44
+
+function checkString(string1,string2){
+
+    if(string1===string2){
+      console.log("yes it's the same");''
+    }
+    else{
+      console.log("not it's not the same");
+    }
+}
+
+//45
+
+function countDigitsInString(str) {
+  let count = 0;
+  for (let i = 0; i < str.length; i++) {
+    let char = str[i];
+    if (char >= '0' && char <= '9') {
+      count++;
+    }
+  }
+  console.log(count);
+}
+
+//46
+
+
+
